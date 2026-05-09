c848163e682ed14cda4c0d78bdae053521290110
bhuvanshu <bhuvanshusingh@gmail.com>
2026-05-09 12:39:32 +0530

    Add backend-java spring boot initialization and project files

---
+++ backend-java/mindcare/src/test/java/com/bhuvanshu/mindcare/MindcareApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bhuvanshu.mindcare;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MindcareApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
